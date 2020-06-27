aca8fbe8b3a24f62f0d7bdd841517df3a3070d64
Vladuka <bogdan_dima@ukr.net>
2020-06-27 16:58:22 +0300

    first commit

---
+++ JavaScript.js
@@ -0,0 +1,22 @@
+/*let b = "hello world";
+//document.write(b);
+console.log(b);
+var LeftBorderWidth = 1;*/
+
+//let arr = [`mudak`,`kozel`,`govno`];
+//document.write(arr[0]);
+//  console.log(arr[1]);
+/*cледующий 004.1 динамическая типизация*/
+//alert("Привіт, світ");
+//let answer = prompt("Ти дурак?");
+//console.log(answer);
+//006 006 Операторы в JS006
+/*let incr = 10,
+/*    decr = 10;
+  console.log(--incr);
+  console.log(++decr);
+  console.log(2==="2");*/
+  let isCheked = false,
+    isClose = false;
+    console.log(isCheked || !isClose);
+    
